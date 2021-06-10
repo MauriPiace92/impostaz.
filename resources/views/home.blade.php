6563aff415eed9252d921d8f4216e48ceed57c58
MauriPiace92 <75382063+MauriPiace92@users.noreply.github.com>
2021-06-10 18:28:31 +0200

    fine Bonus

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,20 +5,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+
+    <link rel="stylesheet" href="{{ asset("css/app.css")}}">
 </head>
 <body>
-    
-    <h1>Ecco i film da vedere!!!!</h1>
+    <div class="container">
+        <h1>I Migliori Film della Storia:</h1>
 
-    <ul>
+        <ul class="menu">
 
-        
-        @foreach ($movies as $movie)
-            <li>
-                <div>{{$movie->title}} - {{$movie->original_title}} - data uscita: {{$movie->date}} - voto: {{$movie->vote}} </div>  
-            </li>                
-        @endforeach
-        
-    </ul>
+            
+            @foreach ($movies as $movie)
+                <li>
+                    <div>{{$movie->title}} - {{$movie->original_title}} - data uscita: {{$movie->date}} - voto: {{$movie->vote}} </div>  
+                </li>                
+            @endforeach
+            
+        </ul>
+    </div>
 </body>
 </html>
